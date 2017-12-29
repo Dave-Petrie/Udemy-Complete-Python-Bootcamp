369c43e4e25a58e4d585d3f84f46621e4dc11133
David Petrie <petriedk@gmail.com>
2017-12-29 22:29:41 +1000

    First commit of actual code. This includes some of the jupyter notebooks that I don't like using.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 Udemy-Complete-Python-Bootcamp
+
+
+This folder has been established to be a code-base for the training and projects I will build through Udemy's:
+
+Complete Python Bootcamp: Go from zero to hero with python
+
+Project started:        17.12.29
+Expected completion:    18.04.01
---
+++ printFormatting.py
@@ -0,0 +1,30 @@
+print("The script loaded fine. Please note this is working with Python 3.")
+
+'''
+PRINT FORMATTING:
+
+print ("Hello World!")
+s = "String";
+print (s)
+
+'''
+#Below is where I am working through the Udemy content
+
+# Define
+x = 5
+a = 13.123456
+# Within strings
+print("Please sir, let me know what x is...:%s" %(x))
+print("Please sir, let me know what a is...:%2.2f" %(a))
+print("Please sir, let me know what a is...:%10.4f" %(a))
+
+# Define
+b = "hello"
+c = "two"
+d = 5
+# Within strings
+print("What is my first, second and third variable: %s, %s and %s" %(b,c,d))
+
+# Ideal python syntax
+print("The first value, V is {V} and the second is {V}".format(V="blah"))
+
