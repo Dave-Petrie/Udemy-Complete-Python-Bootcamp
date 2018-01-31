7aa8fdaa2a9e4b16e8e3daf467d63cbefa315cda
David Petrie <petriedk@gmail.com>
2018-02-01 08:04:31 +1000

    Getting back into studying...Arrived early at the office to study.

--- listComprehensions.py
+++ listComprehensions.py
@@ -11,6 +11,14 @@ for letter in 'word':
     l.append(letter)
 print(l)
 
+lst = [letter for letter in 'word']
+print(lst)
+
+lst2 = [x**2 for x in range(0,11)]
+print(lst2)
+
+lst3 = [number for number in range(11) if number % 2 == 0] #Remeber that % is divide so this 
+
 # DO NOT REMOVE
 
 print("**************************** \n End of script \n ****************************")
